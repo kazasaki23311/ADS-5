46a6d480278c36d7ad8a41572cebce82f12534bf
kazasaki23311 <julia.nikashina@gmail.com>
2026-04-19 22:27:39 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,17 +1,18 @@
 // Copyright 2025 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
 #include <iostream>
 
-template<typename T, int klenOfArr>
+template<typename T, int kSize>
 class TStack {
  private:
-  T arr[klenOfArr];
+  T arr[kSize];
   int top;
  public:
   TStack(): top(-1) {}
   int getSize() const { return top + 1; }
   void push(T x) {
-    if (top + 1 >= klenOfArr) std::cout << "Out of size" << std::endl;
+    if (top + 1 >= kSize) std::cout << "Out of size" << std::endl;
     else arr[++top] = x;
   }
   T pop() {
